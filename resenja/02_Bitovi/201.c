182e264469011a47028c8143a1cca8f93d5f3d54
ninaradojicic <ninaradojicic@gmail.com>
2015-08-17 14:48:38 +0200

    Promene u tekstovima zadataka za prva dva poglavlja.

--- resenja/02_Bitovi/201.c
+++ resenja/02_Bitovi/201.c
@@ -2,7 +2,8 @@
 
 int main(){
   int a, b;
-  printf("Hello bitovi!\n"); /* Da li komentari rade čćžšđ*/
+  printf("Hello bitovi!\n"); 
+  /* Da li komentari rade čćžšđ*/
   if(a>b) a++;
   return 0;
 }
