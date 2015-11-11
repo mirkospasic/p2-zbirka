2a6180235a7456f4c1dad13e7c06b61cb2dc3e17
ninaradojicic <ninaradojicic@gmail.com>
2015-11-11 23:01:41 +0100

    Finalne ispravke pred recenziju

--- resenja/00_PodelaPoDatotekama/002/main.c
+++ resenja/00_PodelaPoDatotekama/002/main.c
@@ -1,8 +1,8 @@
-/**************************************************************
+/********************************************************************
 Ovaj program koristi korektno definisanu biblioteku kompleksnih 
-brojeva. U zaglavlju complex.h nalazi se definicija komplesnog 
-broja i popis deklaracija podrzanih funkcija, a u complex.c se 
-nalaze njihove definicije.
+brojeva. U zaglavlju complex.h nalazi se definicija komplesnog broja 
+i popis deklaracija podrzanih funkcija, a u complex.c se nalaze 
+njihove definicije.
 
 Kompilacija programa se najjednostavnije postize naredbom
 gcc -Wall -lm -o izvrsni complex.c main.c 
@@ -11,7 +11,13 @@ Kompilacija se moze uraditi i na sledeci nacin:
 gcc -Wall -c -o complex.o complex.c
 gcc -Wall -c -o main.o main.c
 gcc -lm -o complex complex.o main.o
-**************************************************************/
+
+Napomena: Prethodne komande se koriste kada se sva tri navedena 
+dokumenta nalaze u istom direktorijumu. Ukoliko se biblioteka (npr. 
+complex.c complex.h) nalazi u direktorijumu header_dir prevodjenje 
+se vrsi koriscenjem opcije
+-I header_dir
+********************************************************************/
 
 
 #include <stdio.h>
---
+++ resenja/09_IspitniRokovi/stabla.h
@@ -0,0 +1,50 @@
+#ifndef _STABLA_H_
+#define _STABLA_H_ 1
+
+    /* a) Struktura kojom se predstavlja cvor binarnog
+       pretrazivackog stabla */ 
+    typedef struct cvor {
+  int broj;
+   struct cvor *levo;
+  struct cvor *desno;
+} Cvor;
+
+    /* b) Funkcija koja alocira memoriju za novi cvor stabla,
+       inicijalizuje polja strukture i vraca pokazivac na novi cvor */ 
+    Cvor * napravi_cvor(int broj);
+
+    /* c) Funkcija koja dodaje zadati broj u stablo. Povratna
+       vrednost funkcije je 0 ako je dodavanje uspesno, odnosno 1
+       ukoliko je doslo  do greske */ 
+int dodaj_u_stablo(Cvor ** adresa_korena, int broj);
+
+    /* d) Funkcija koja proverava da li se zadati broj nalazi stablu */ 
+    Cvor * pretrazi_stablo(Cvor * koren, int broj);
+
+    /* e) Funkcija koja pronalazi cvor koji sadrzi najmanju vrednost
+       u stablu */ 
+    Cvor * pronadji_najmanji(Cvor * koren);
+
+    /* f) Funkcija koja pronalazi cvor koji sadrzi najvecu vrednost
+       u stablu */ 
+    Cvor * pronadji_najveci(Cvor * koren);
+
+    /* g) Funkcija koja brise cvor stabla koji sadrzi zadati broj */ 
+void obrisi_element(Cvor ** adresa_korena, int broj);
+
+    /* h) Funkcija koja ispisuje stablo u infiksnoj notaciji (Levo
+       postablo - Koren - Desno podstablo ) */ 
+void ispisi_stablo_infiksno(Cvor * koren);
+
+    /* i) Funkcija koja ispisuje stablo u prefiksnoj notaciji ( Koren 
+       - Levo podstablo - Desno podstablo ) */ 
+void ispisi_stablo_prefiksno(Cvor * koren);
+
+    /* j) Funkcija koja ispisuje stablo postfiksnoj notaciji ( Levo
+       podstablo - Desno postablo - Koren) */ 
+void ispisi_stablo_postfiksno(Cvor * koren);
+
+    /* k) Funkcija koja oslobadja memoriju zauzetu stablom.  */ 
+void oslobodi_stablo(Cvor ** adresa_korena);
+
+#endif  /*  */
